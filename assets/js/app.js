de226b31e7cf6bcfa669f32637718ef1e4bed278
Jatin Pasrija <jatinpasrija@Jatins-MacBook-Pro.local>
2025-09-10 19:08:35 +0530

    Updates in code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 
-    fetch('/assets/data/products.json')
+    fetch('/landing-website/assets/data/products.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error("HTTP error " + response.status);
